9e873ed7e8c6db105bba5742e79ab4dfc08af806
Ahnaf-Alam <ahnafalam15@gmail.com>
2026-09-13 21:32:16 +0600

    remove previous order service

--- e-commerce/order-service/src/main/java/com/techilam/order_service/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.techilam.order_service.repository;
-
-import com.techilam.order_service.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
